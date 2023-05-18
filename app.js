5ce741f018e24ffa794cc40edc066ce827359e03
Austin Lovold <austin@lovolds.com>
2023-05-18 11:41:53 -0500

    completed exercise

--- app.js
+++ app.js
@@ -4,7 +4,25 @@ console.log(
 );
 
 // Exercise 1
-
 // Exercise 2
-
+const firstName = "Austin";
+let lastName = "Lovold";
+var age = 30;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+//Ex4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+//Ex5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
